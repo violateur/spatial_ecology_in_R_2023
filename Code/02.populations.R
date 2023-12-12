269c581027c36677a185e90687af923fd73d0ac9
violateur <147074198+violateur@users.noreply.github.com>
2023-12-12 11:57:32 +0100

    Update 02.populations.R

--- Code/02.populations.R
+++ Code/02.populations.R
@@ -1,20 +1,26 @@
-# uinstall.pcakages("spatstat")
+install.pcakages("spatstat")
 library(spatstat)
 library(terra)
 
+# let's use the bei data:
+# data description:
+# https://CRAN.R-project.org/package=spatstat
 bei 
 plot(bei) 
 plot(bei, pch=19, cex=.5) 
 
+#select the elements
 bei.extra
 plot(bei.extra)
 plot(bei.extra[[1]])
 
+#second way to select the elements
 plot(bei.extra[[1]])
 plot(bei, pch=19, cex=.5, add=T) 
 
 density_map <- density(bei)
 
+#multiframe
 par(mfrow=c(2,1))
 plot(bei.extra[[1]])
 plot(density_map)
