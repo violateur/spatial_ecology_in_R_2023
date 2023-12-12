4201aec86e5f40da2eb7d3ef7ba22354b3c55c3f
violateur <147074198+violateur@users.noreply.github.com>
2023-12-12 12:36:34 +0100

    Update 04.remotesensing.visualization.R

--- Code/04.remotesensing.visualization.R
+++ Code/04.remotesensing.visualization.R
@@ -1,10 +1,21 @@
 # This is a script to visualize satellite data
 
-library(devtools) 
+install.packages("devtools")
+install.packages("terra")
 
+library(devtools)
+library(terra)
+library(imageRy)
+
+devtools::install_github("ducciorocchini/imageRy")
+
+im.list()
+
+b2 <- im.import("sentinel.dolomites.b2.tif")
 # install the imageRy package from GitHub
 devtools::install_github("ducciorocchini/imageRy")
 
+###############
 library(imageRy)
 library(terra)
 
